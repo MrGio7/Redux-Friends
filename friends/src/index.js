34b2534cc4058b9a2cd2bfdef02c92390ed0ab3a
MrGio7 <lashagiorgi1233@gmail.com>
2019-05-22 21:06:55 +0400

    imported redux logger

--- friends/src/index.js
+++ friends/src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import reducer from './reducers';
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk, logger)));
 
 const rootElement = document.getElementById('root');
 ReactDOM.render(
