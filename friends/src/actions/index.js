dcc9f344667b4343edadda9b6a71bfc516a472f4
MrGio7 <lashagiorgi1233@gmail.com>
2019-05-23 15:01:40 +0400

    imported login action

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -1,15 +1,25 @@
 import axios from 'axios';
 
+export const LOGIN_START = 'LOGIN_START';
+export const login = creds => dispatch => {
+    dispatch({ type: LOGIN_START });
+    return axios.post('http://localhost:5000/api/login', creds).then(res => {
+        localStorage.setItem('token', res.data.payload)
+    })
+}
+
 export const FETCH_DATA_START = 'FETCH_DATA_START';
 export const FETCH_DATA_SUCCESS = 'FETCH_DATA_SUCCESS';
 export const FETCH_DATA_FAILURE = 'FETCH_DATA_FAILURE';
 export const getData = () => dispatch => {
     dispatch({ type: FETCH_DATA_START });
     axios
-    .get('http://localhost:5000/api/friends')
+    .get('http://localhost:5000/api/friends', {
+        headers: { Authorization: localStorage.getItem('token')}
+    })
     .then(res => {
         console.log(res);
-        dispatch({ type: FETCH_DATA_SUCCESS })
+        dispatch({ type: FETCH_DATA_SUCCESS, payload: res.data.friends })
     })
     .catch (err => {
         console.log(err);
