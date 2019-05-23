8b8fcab2989c9ca3a62b0143cb95592cfa9f3794
MrGio7 <lashagiorgi1233@gmail.com>
2019-05-23 14:24:39 +0400

    imported friend list component

--- friends/src/components/friends.js
+++ friends/src/components/friends.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import { getData } from '../actions';
 
 class Friends extends React.Component {
 
     render(){
-        <div className='friends'>
-            <h2>Friend List</h2>
-        </div>
+        return(
+            <div className='friends'>
+                <h2>Friend List</h2>
+            </div>
+        )
     }
 }
+
+export default withRouter(
+    connect(
+        null,
+        { getData }
+    )(Friends)
+)
 
