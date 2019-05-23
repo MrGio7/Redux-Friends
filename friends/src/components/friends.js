53bb1320040c40b7f7d2da6cd8930a538f96d3af
MrGio7 <lashagiorgi1233@gmail.com>
2019-05-23 16:52:26 +0400

    added styling to friend list

--- friends/src/components/friends.js
+++ friends/src/components/friends.js
@@ -12,12 +12,11 @@ class Friends extends React.Component {
     render(){
         return(
             <div className='friends'>
-                <h2>Friend List</h2>
                     {this.props.friends.map((friend, index) => (
-                        <div className='friendName'>
-                                <h3>{friend.name}</h3>
-                                <h3>{friend.age}</h3>
-                                <h3>{friend.email}</h3>
+                        <div className='friend'>
+                                <h3>Name: {friend.name}</h3>
+                                <h3>Age: {friend.age}</h3>
+                                <h3>Mail: {friend.email}</h3>
                         </div>
                     ))}
             </div>
